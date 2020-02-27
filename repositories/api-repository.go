821d1d105fe4bafbbbd4ba087e8455a80eecae7f
florian.charpentier <florian.charpentier@epita.fr>
2020-02-27 15:06:16 +0100

    added CRUD on api (repo only)

--- repositories/api-repository.go
+++ repositories/api-repository.go
@@ -29,3 +29,41 @@ func ApiGet(apiName string) (models.ApiModel, string, error) {
 
 	return a, "api retrieved", nil
 }
+
+func ApiCreate(a models.ApiModel) (models.ApiModel, string, error) {
+	if apiExists(a) {
+		msg := "existing api with same name"
+		return models.ApiModel{}, msg, errors.New(msg)
+	}
+
+	api.Api.Database.Orm.Create(&a)
+
+	if !apiExists(a) {
+		msg := "failed to create api"
+		return models.ApiModel{}, msg, errors.New(msg)
+	}
+
+	return a, "api created", nil
+}
+
+func ApiUpdate(a models.ApiModel) (models.ApiModel, string, error) {
+	if !apiExists(a) {
+		msg := fmt.Sprintf("no api with name %s found", a.Name)
+		return models.ApiModel{}, msg, errors.New(msg)
+	}
+
+	api.Api.Database.Orm.Save(&a)
+
+	return a, "api updated", nil
+}
+
+func ApiDelete(a models.ApiModel) (models.ApiModel, string, error) {
+	if !apiExists(a) {
+		msg := fmt.Sprintf("no api with name %s found", a.Name)
+		return models.ApiModel{}, msg, errors.New(msg)
+	}
+
+	api.Api.Database.Orm.Delete(&a)
+
+	return a, "api deleted", nil
+}
